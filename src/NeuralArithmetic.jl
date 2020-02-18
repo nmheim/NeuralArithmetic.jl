d35c8f65f30c711262014b595ae7b61f5e368551
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2020-02-18 10:32:36 +0100

    remove NALUX from master. closes #2

--- src/NeuralArithmetic.jl
+++ src/NeuralArithmetic.jl
@@ -3,11 +3,9 @@ module NeuralArithmetic
 using Flux
 using Zygote
 using Flux: glorot_uniform
-using LinearAlgebra: Diagonal
 
 include("nac.jl")
 include("nalu.jl")
-include("nalux.jl")
 
 include("nau.jl")
 include("nmu.jl")
