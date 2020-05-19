62bccf7538861b0f842b650231bb26199791470a
Niklas Heim <heim.niklas@gmail.com>
2020-05-19 15:16:21 +0200

    whoopsie

--- src/NeuralArithmetic.jl
+++ src/NeuralArithmetic.jl
@@ -8,12 +8,10 @@ using DiffEqFlux
 include("nau.jl")
 include("nmu.jl")
 include("npu.jl")
+include("fast_npu.jl")
 
 include("nac.jl")
 include("nalu.jl")
 include("nalux.jl")
-
-include("fast_nau.jl")
-include("fast_npu.jl")
 
 end # module
